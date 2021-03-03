c9d1b3c04b96b1339796d3f94117cefc88809a22
Maximilian Kapra <maximilian@kapra.de>
2021-03-03 20:59:11 +0100

    Initial Commit
    This commit contains:
    * Initial Rails setup
    * Devise login with username
    * Bootstrap, jQuery

---
+++ db/migrate/20210303192631_devise_create_users.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class DeviseCreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      t.timestamps null: false
+    end
+
+    add_column :users, :username, :string, {unique: true, null: false}
+
+    add_index :users, :username,             unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+  end
+end
