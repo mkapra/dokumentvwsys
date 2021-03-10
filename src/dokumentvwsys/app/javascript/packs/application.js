a09549a71e4c02b9329588adb6ad16e601096b72
Restyled.io <commits@restyled.io>
2021-03-10 13:11:33 +0000

    Restyled by prettier

--- src/dokumentvwsys/app/javascript/packs/application.js
+++ src/dokumentvwsys/app/javascript/packs/application.js
@@ -3,11 +3,11 @@
 // logic in a relevant structure within app/javascript and only use these pack
 // files to reference that code so it'll be compiled.
 
-global.toastr = require("toastr")
+global.toastr = require("toastr");
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("bootstrap")
-require("bootstrap-datepicker")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("bootstrap");
+require("bootstrap-datepicker");
