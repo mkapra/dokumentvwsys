f6de72bd54494fdfc9ea4b4966b8c629033e835d
Maximilian Kapra <maximilian@kapra.de>
2021-03-11 21:46:41 +0100

    Initial commit for administration panel
    * Add bootstrap icons
    * Views for administration

--- src/dokumentvwsys/app/controllers/administration_controller.rb
+++ src/dokumentvwsys/app/controllers/administration_controller.rb
@@ -4,16 +4,30 @@ class AdministrationController < ApplicationController
 
   def index
     @users = User.all
+    @user = User.new
   end
 
   def new
     @user = User.new
   end
 
+  def edit
+  end
+
   def create
   end
 
+  def destroy
+    redirect_to administration_index_path, flash: { error: 'Cannot delete yourself' } if params[:id].to_i == current_user.id
+
+    User.find(params[:id]).destroy
+    flash[:notice] = 'User deleted successfully'
+    redirect_to action: 'index'
+  rescue ActiveRecord::RecordNotFound
+    redirect_to action: 'index', flash: { error: 'User not found' }
+  end
+
   def verify_admin_user
-    redirect_to root_path, flash: { error: "Not allowed" } unless current_user.admin?
+    redirect_to root_path, flash: { error: 'Not allowed' } unless current_user.admin?
   end
 end
