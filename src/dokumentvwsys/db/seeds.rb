4dc490d9ff890125d362964a4957bd61366a060d
Restyled.io <commits@restyled.io>
2021-03-11 21:44:35 +0000

    Restyled by rubocop

--- src/dokumentvwsys/db/seeds.rb
+++ src/dokumentvwsys/db/seeds.rb
@@ -1,4 +1,4 @@
-PROGRAM_TITLE = "dokumentvwsys"
+PROGRAM_TITLE = 'dokumentvwsys'
 
 # Add preferences
 title = Preference.new(key: 'title', value: PROGRAM_TITLE, description: 'preferences.title', group: 'basic')
--- src/dokumentvwsys/test/models/preference_test.rb
+++ src/dokumentvwsys/test/models/preference_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class PreferenceTest < ActiveSupport::TestCase
   # test "the truth" do
