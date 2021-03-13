579a148274834fc852bbd1de6a26763248ad0b16
Maximilian Kapra <maximilian@kapra.de>
2021-03-13 19:33:06 +0100

    Add translations for login page

--- src/dokumentvwsys/app/models/user.rb
+++ src/dokumentvwsys/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :validatable
 
   has_many :documents
 
