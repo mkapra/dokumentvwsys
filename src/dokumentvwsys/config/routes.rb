fde48c2cf53f6a7e187a0ce20be1d98767aa3b98
Maximilian Kapra <maximilian@kapra.de>
2021-03-25 22:06:47 +0100

    Preferences: Add file upload for field
    --- Why is this change necessary?
    Some Preferences may require an uploaded file. So we need to have
    a binary field in the database to store this file.
    
    --- How does it address the issue?
    1. Add binary column and boolean column to specify if it should be a file or
       not
    2. Add file browser to view
    3. Add dummy logo to fixtures
    
    Also:
    4. Refactor locales for Preferences and therefore delete the description
       column.

--- src/dokumentvwsys/config/routes.rb
+++ src/dokumentvwsys/config/routes.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :preferences
   resources :administration
+
+  resources :preferences do
+    get 'logo', to: 'preferences#logo', on: :collection, as: 'logo'
+  end
 
   get 'documents/all', to: 'documents#all'
   resources :documents
