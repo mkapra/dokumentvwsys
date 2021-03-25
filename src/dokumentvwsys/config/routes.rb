5512009e4574354a96ac83df8b81dce66a970d0f
Restyled.io <commits@restyled.io>
2021-03-25 22:08:29 +0000

    Restyled by rubocop

--- src/dokumentvwsys/config/routes.rb
+++ src/dokumentvwsys/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'documents/all', to: 'documents#all'
   resources :documents
 
-  #get 'registration/pdf/:id', to: 'registration#pdf', as: 'pdf_registration'
+  # get 'registration/pdf/:id', to: 'registration#pdf', as: 'pdf_registration'
 
   devise_for :users, controllers: {
     registrations: 'users/registrations'
