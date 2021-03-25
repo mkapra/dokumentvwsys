6bc4f31c870d4360e596208ab407c10589c1ade7
Maximilian Kapra <maximilian+github@kapra.de>
2021-03-25 23:16:05 +0100

    Test: Add test for administration controller and refactor code (#57)
    * Test: Add test for administration controller and refactor code
    
    Added Tests and fixed bugs that appeared while testing
    
    * Restyled by rubocop (#58)
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/dokumentvwsys/app/controllers/users/registrations_controller.rb
+++ src/dokumentvwsys/app/controllers/users/registrations_controller.rb
@@ -2,8 +2,10 @@
 
 module Users
   class RegistrationsController < Devise::RegistrationsController
+    include AdministrationHelper
+
     skip_before_action :require_no_authentication
-    before_action :check_permission, only: %i[new create cancel]
+    before_action :verify_admin_user, only: %i[new create cancel]
 
     # before_action :configure_sign_up_params, only: [:create]
     # before_action :configure_account_update_params, only: [:update]
@@ -18,16 +20,10 @@ module Users
       new_user = User.new(user_params)
       return redirect_to new_administration_path, flash: { error: 'Error while creating the user' } unless new_user.save
 
-      redirect_to new_administration_path, flash: { notice: 'User created successfully' }
+      redirect_to new_administration_path, flash: { notice: t('message.notice.create_user') }
     end
 
     protected
-
-    def check_permission
-      return redirect_to root_path, flash: { error: 'Not authorized' } if current_user.nil?
-
-      redirect_to root_path, flash: { error: 'Not authorized' } unless current_user.admin?
-    end
 
     def user_params
       params.require(:user).permit(:username, :first_name, :last_name, :birth, :password, :password_confirmation,
