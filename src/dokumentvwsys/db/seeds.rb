1cadf8accad4d98b8c0aabff619580167e22477c
Maximilian Kapra <maximilian@kapra.de>
2021-03-26 11:33:51 +0100

    Remove unnecessary description field

--- src/dokumentvwsys/db/seeds.rb
+++ src/dokumentvwsys/db/seeds.rb
@@ -1,8 +1,8 @@
 PROGRAM_TITLE = 'dokumentvwsys'
 
 # Add preferences
-Preference.create(key: 'title', value: PROGRAM_TITLE, description: 'preferences.description.title', group: 'basic')
-Preference.create(key: 'url', value: '', description: 'preferences.description.url', group: 'basic')
+Preference.create(key: 'title', value: PROGRAM_TITLE, group: 'basic')
+Preference.create(key: 'url', value: '', group: 'basic')
 Preference.create(key: 'image', is_file: true, group: 'basic') # TODO: Add default image #7
 
 Role.create(name: 'admin')
