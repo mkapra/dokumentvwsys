d6c66d7c5268458b109751cf81c8445bf5421407
Maximilian Kapra <maximilian+github@kapra.de>
2021-03-28 14:07:09 +0200

    Preferences: Add textarea column for footer (#79)
    * Preferences: Add textarea column for footer
    
    --- Why is this change necessary?
    We need a textarea for larger texts.
    
    --- How does it address the issue?
    Add is_textarea boolean.
    Also add the footer preference.
    
    --- Known side effects?
    We need a validation of the length of the textarea.
    
    * Restyle Preferences: Add textarea column for footer (#80)
    
    * Restyled by prettier-yaml
    
    * Restyled by rubocop
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/dokumentvwsys/app/models/preference.rb
+++ src/dokumentvwsys/app/models/preference.rb
@@ -1,5 +1,8 @@
 class Preference < ApplicationRecord
   def to_s
+    return '' if is_file
+    return value.html_safe if is_textarea
+
     value
   end
 end
--- src/dokumentvwsys/db/seeds.rb
+++ src/dokumentvwsys/db/seeds.rb
@@ -1,8 +1,13 @@
 PROGRAM_TITLE = 'dokumentvwsys'
 
+FOOTER_DEFAULT_TEXT = <<~EOF
+  dokumentvwsys ist ein System zur Bereitstellung von Dokumenten.
+EOF
+
 # Add preferences
 Preference.create(key: 'title', value: PROGRAM_TITLE, group: 'basic')
 Preference.create(key: 'image', is_file: true, group: 'basic') # TODO: Add default image #7
+Preference.create(key: 'footer', textarea: FOOTER_DEFAULT_TEXT, is_textarea: true, group: 'appearance')
 
 Role.create(name: 'admin')
 Role.create(name: 'uploader')
