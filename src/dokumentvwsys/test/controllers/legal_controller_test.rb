edf96ef5b8527e1ca4d99e8b3b069ef6ae3cebe7
Restyled.io <commits@restyled.io>
2021-06-16 19:57:37 +0000

    Restyled by rubocop

--- src/dokumentvwsys/test/controllers/legal_controller_test.rb
+++ src/dokumentvwsys/test/controllers/legal_controller_test.rb
@@ -1,12 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class LegalControllerTest < ActionDispatch::IntegrationTest
-  test "should get imprint" do
+  test 'should get imprint' do
     get legal_imprint_url
     assert_response :success
   end
 
-  test "should get privacy_policy" do
+  test 'should get privacy_policy' do
     get legal_privacy_policy_url
     assert_response :success
   end
