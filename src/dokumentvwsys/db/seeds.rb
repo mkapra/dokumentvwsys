eb89d11167b78e6439c764fb1790192fa7517b28
Maximilian Kapra <maximilian@kapra.de>
2021-06-16 21:57:21 +0200

    🎨 Add missing seed for privacy policy

--- src/dokumentvwsys/db/seeds.rb
+++ src/dokumentvwsys/db/seeds.rb
@@ -10,6 +10,7 @@ Preference.create(key: 'url', value: '', group: 'basic')
 Preference.create(key: 'image', is_file: true, group: 'basic') # TODO: Add default image #7
 Preference.create(key: 'footer', textarea: FOOTER_DEFAULT_TEXT, is_textarea: true, group: 'appearance')
 Preference.create(key: 'imprint', value: 'Test Imprint', is_textarea: true, group: 'legal')
+Preference.create(key: 'privacy_policy', value: 'Test Privacy Policy', is_textarea: true, group: 'legal')
 
 Role.create(name: 'admin')
 Role.create(name: 'uploader')
