22486cc4a2cff23f3eca5a36479e389b1d9c8457
Adnan Al Shorbaji <40066118+adnansh95@users.noreply.github.com>
2021-06-24 00:01:58 +0200

    Add download function to administration index page (#129)
    * Add download function to administration index page
    
    * Apply suggestions from code review
    
    Co-authored-by: Maximilian Kapra <maximilian+github@kapra.de>
    
    * restyling :)
    
    Co-authored-by: Maximilian Kapra <maximilian+github@kapra.de>

--- src/dokumentvwsys/db/schema.rb
+++ src/dokumentvwsys/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_135735) do
+ActiveRecord::Schema.define(version: 2021_06_22_192049) do
 
   create_table "documents", force: :cascade do |t|
     t.binary "pdf", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2021_03_26_135735) do
     t.string "last_name"
     t.date "birth"
     t.integer "role_id"
+    t.boolean "downloaded", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
