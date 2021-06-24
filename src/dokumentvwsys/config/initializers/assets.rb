2477b7cb9a24a24a6e926f91f8e74cad7117ca51
Maximilian Kapra <maximilian@kapra.de>
2021-06-24 16:37:14 +0200

    Add number_pages

--- src/dokumentvwsys/config/initializers/assets.rb
+++ src/dokumentvwsys/config/initializers/assets.rb
@@ -11,5 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w(pdf.css pdf.js blueprint/screen.css)
+Rails.application.config.assets.precompile += %w(pdf.css pdf.js blueprint/screen.css number_pages.js)
