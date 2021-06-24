b7358cdf7555df1c46b53c1717ec3ed28c18a55b
Maximilian Kapra <maximilian@kapra.de>
2021-06-24 16:45:38 +0200

    Remove page_numbers from pdf

--- src/dokumentvwsys/config/initializers/assets.rb
+++ src/dokumentvwsys/config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w(pdf.css pdf.js blueprint/screen.css number_pages.js)
+Rails.application.config.assets.precompile += %w(pdf.css pdf.js blueprint/screen.css)
